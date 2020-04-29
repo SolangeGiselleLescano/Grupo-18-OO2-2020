9350984a4e29894f7306fde8928bac6fa2515952
SolangeLescano <43324635+SolangeGiselleLescano@users.noreply.github.com>
2020-04-29 20:33:12 -0300

    Update Grupo-18-BDD-OO2-2020 .sql

--- Grupo-18-BDD-OO2-2020 .sql
+++ Grupo-18-BDD-OO2-2020 .sql
@@ -15,7 +15,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema bd-sistema
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `bd-sistema` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `bd-sistema` ;
+USE `Grupo-18-BDD-OO2-2020` ;
 
 -- -----------------------------------------------------
 -- Table `bd-sistema`.`persona`
